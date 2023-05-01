dc3c5d689efdcf0343b7c4365c81b07fd5674980
fadeyjo <komrad.gubi2017@yandex.ru>
2023-05-01 19:16:50 +0300

    QSS style

--- frontend/startWidget.py
+++ frontend/startWidget.py
@@ -28,6 +28,61 @@ class myApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    
+    app.setStyleSheet("""
+    QLineEdit
+    {
+        background-color: #5b586e;
+        border-radius: 10px;
+        padding: 5px;
+        border: 1px solid #5b586e;
+        color: #fff;
+    }
+
+    QPushButton
+    {
+        background-color: #0d6efd;
+        border: 1px solid #343155;
+        color: #fff;
+        padding 6px;
+        margin-top: 10px;
+        border-radius: 8px;
+    }
+
+    QPushButton:hover,
+    QPushButton:clicked
+    {
+        background-color: #0b5ed7;
+        border: 1px solid #9ac3fe;
+    }
+
+    QTimeEdit
+    {
+        background-color: #5b586e;
+        border-radius: 10px;
+        padding: 5px;
+        border: 1px solid #5b586e;
+        color: #fff;
+    }
+
+    QDateEdit
+    {
+        background-color: #5b586e;
+        border-radius: 10px;
+        padding: 5px;
+        border: 1px solid #5b586e;
+        color: #fff;
+    }
+
+    QComboBox
+    {
+        background-color: #5b586e;
+        border-radius: 10px;
+        padding: 5px;
+        border: 1px solid #5b586e;
+        color: #fff;
+    }
+    """)
 
     newApp = myApp()
     newApp.show()
